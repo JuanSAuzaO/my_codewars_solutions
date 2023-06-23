eec152030d9653371834d0f6ad8e348f6aa2f90c
JuanSAuzaO <sebastianauza@hotmail.com>
2023-06-23 17:53:17 -0500

    Refactored for performance and added a detailed explanation to permute a palindrome kata solution

--- permute_palindrome.rb
+++ permute_palindrome.rb
@@ -1,43 +1,29 @@
+#Link to the challenge to check instructions: https://www.codewars.com/kata/58ae6ae22c3aaafc58000079
+
 def permute_a_palindrome(input)
-  even = []
-  odd = []
-  if input.length.odd?
-    input.each_char do |char|
-      if !input.count(char).even?
-        odd.push(true)
+  odd = {} #1 Here we will store "true" for any character that may have an odd count in the input.
+  if input.length.odd? #2 We verify if the input has an even or an odd length.
+    input.each_char do |char| #3.a In case it is odd we need to get sure there is only 1 character with an odd count.
+      if !input.count(char).even? # So we iterate over the input checking for odd count characters, in such case, we add them to the hash. 
+        odd[char] ||= true
       end
     end
-    if odd.length > 1
-      even = []
-      middle_index = (input.length - 1) / 2
-      input.slice!(middle_index)
-      input.each_char do |char|
-        if
-          even.push(true)
-        else
-          even.push(false)
-        end
-        if even.include?(false)
-          return false
-        else
-          return true
-        end
-      end
-    else
+    if odd.length > 1 # 4.a If for some reason there is more than one odd count character, we cannot permute palindromes, so we return false.
+      return false
+    else # If there is only one odd count character, we can permute palindromes.
       return true
     end
   else
-    input.each_char do |char|
-      if input.count(char).even?
-        even.push(true)
-      else
-        even.push(false)
+    input.each_char do |char| #3.b In case input length is even we need to get sure there is not a single odd count character.
+      if !input.count(char).even? # So we iterate over the input checking for an odd count character, if it exists, we add it to the hash and stop the loop.
+        odd[char] = true
+        break
       end
     end
-    if even.include?(false)
-      return false
+    if odd.length > 0 # 4.b If there is an odd count character, we cannot permute palindromes.
+      return false 
     else
-      return true
+      return true # If there are not any odd count characters, we can permute palindromes.
     end
   end
 end
