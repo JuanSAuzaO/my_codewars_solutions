720e31bf05acc0e3473a03700634d5ab7806b436
JuanSAuzaO <sebastianauza@hotmail.com>
2023-06-29 13:07:15 -0500

    Refactored for efficiency and added detailed explanation to first non-repeating character kata solution

--- first_non_repeated.rb
+++ first_non_repeated.rb
@@ -1,12 +1,15 @@
+# Link to challenge instructions: https://www.codewars.com/kata/52bc74d4ac05d0945d00054e
+
 def  first_non_repeating_letter(s)
-  count = {}
-  copy = s.downcase
+  copy = s.downcase # 1 We save a lower case copy of the input string.
   
-  s.each_char do |char|
-    if copy.count(char.downcase) == 1
-      return count[1] = char
+  s.each_char do |char| # 2 We start an iteration over the input string.
+    # We check the count of each character. Character must only appear once:
+    if copy.count(char.downcase) == 1 # Appearances are case insensitive.
+      # But anyway we must return the character in the original case.
+      return char
     end
   end
-  
-  return ""
+  # If a string contains all repeating characters or a invalid input, we return nothing.
+  return "" 
 end
